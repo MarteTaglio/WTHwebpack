25a4d9d2bd3b240550a198d7c5cf9db0c1b5675a
MartinoTagliente <martinotagliente@outlook.it>
2019-09-21 16:36:09 +0200

    axios e chiamata esempio

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import $ from "jquery";
 import "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+const axios = require('axios').default;
 
-
-$(function () {
-  $('.example-popover').popover({
-    container: 'body'
+function fetchTest() {
+axios.post('https://whatsthehit.herokuapp.com/api/select', {
+  "from": "canzone",
+  "select": "titolo",
+  "where": {
+    "anno": "1980"}
   })
+  .then(function (response) {
+    document.body.innerText = JSON.stringify(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 
-})
+document.getElementById("bottone").addEventListener("click", fetchTest);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+var FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
 
 module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
   entry: "./src/index.js",
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
-    open: true
+    open: true,
+    quiet: true
   },
   module: {
     rules: [
@@ -19,6 +24,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new FriendlyErrorsWebpackPlugin(),
     new CopyPlugin([
       { from: 'static', to: './' }
     ]),
