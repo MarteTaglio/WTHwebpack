1f177f6809d50e57551d48d5ffc97421fd1dd0e9
Stefano <stefano@tambellini.it>
2019-09-25 20:07:12 +0200

    WIP: Bulma funzionante con build (errore  serve:dev!)

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,53 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-var FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
   entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "dist"
   },
   devServer: {
     open: true,
-    quiet: true
+    quiet: true,
+    contentBase: path.resolve(__dirname),
+    historyApiFallback: true,
+    publicPath: "dist"
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+              // options...
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
     new FriendlyErrorsWebpackPlugin(),
-    new CopyPlugin([
-      { from: 'static', to: './' }
-    ]),
-  ],
+    new CopyPlugin([{ from: "static", to: "./" }]),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css"
+    })
+  ]
 };
