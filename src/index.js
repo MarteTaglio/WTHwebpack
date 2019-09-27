fa14557251eb8ae236af92a99444c79c71591d5c
MartinoTagliente <martinotagliente@outlook.it>
2019-09-27 12:02:33 +0200

    Popup page + main title

--- src/index.js
+++ src/index.js
@@ -54,3 +54,14 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+
+var modal = document.getElementById('modal');
+  var elements = document.getElementsByClassName('toggle-modal');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', toggleClass);
+  }
+
+  function toggleClass() {
+    modal.classList.toggle('is-active');
+  }
